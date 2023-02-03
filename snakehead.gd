18a13cf6091b68adaf24e23c7caaae8436f5da0b
user <user@goliath.dbai.tuwien.ac.at>
2023-02-03 19:42:08 +0100

    Head moves in angle direction.

--- snakehead.gd
+++ snakehead.gd
@@ -9,39 +9,30 @@ enum Dir {
 	UP, DOWN, LEFT, RIGHT
 } 
 
-var GRID_SIZE = 16.0
-
 var VEL = 64.0
-var dir = Dir.RIGHT
-var next_dir = Dir.RIGHT
+var angle = 0.0
+var turn_speed = 180.0
 
 func _ready():
 	pass
 
 var timer = 0.0
-var CHANGE_TIME = GRID_SIZE/VEL
+var SPAWN_TIME = 1
 
 func _process(delta):
 	timer += delta
 	
-	if Input.is_action_just_pressed("right"):
-		dir = Dir.RIGHT
-	elif Input.is_action_just_pressed("down"):
-		dir = Dir.DOWN
-	elif Input.is_action_just_pressed("left"):
-		dir = Dir.LEFT
-	elif Input.is_action_just_pressed("up"):
-		dir = Dir.UP
-		
-	if dir == Dir.RIGHT:
-		position.x += VEL*delta
-		$Sprite.rotation_degrees = 0
-	elif dir == Dir.DOWN: 
-		position.y += VEL*delta
-		$Sprite.rotation_degrees = 90
-	elif dir == Dir.LEFT: 
-		position.x -= VEL*delta
-		$Sprite.rotation_degrees = 180
-	elif dir == Dir.UP: 
-		position.y -= VEL*delta
-		$Sprite.rotation_degrees = 270
+	if Input.is_action_pressed("right"):
+		angle += turn_speed * delta
+	elif Input.is_action_pressed("left"):
+		angle -= turn_speed * delta
+	
+	if angle > 360:
+		angle -= 360
+	elif angle < 0:
+		angle += 360
+	
+	print(angle)
+	
+	$Sprite.rotation_degrees = angle
+	position += Vector2(1, 0).rotated(deg2rad(angle)) * VEL * delta
