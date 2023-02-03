5e453caa6f49e0b20d4653e4fb871e9b1ac5d6e2
user <user@goliath.dbai.tuwien.ac.at>
2023-02-04 00:05:44 +0100

    No snake body collisions and snakes go down

--- CameraControl.gd
+++ CameraControl.gd
@@ -4,4 +4,4 @@ extends Node2D
 func _process(delta):
 	var snake_1_pos = get_parent().get_node("snakehead1").position
 	var snake_2_pos = get_parent().get_node("snakehead2").position
-	position.y = snake_1_pos.linear_interpolate(snake_2_pos, 0.5).y
+	position.y = max(position.y, snake_1_pos.linear_interpolate(snake_2_pos, 0.5).y)
