07987ef7a7d7f3d22cca114846729eebbb45974b
user <user@goliath.dbai.tuwien.ac.at>
2023-02-04 14:59:22 +0100

    Out of camera bounds game restart.

--- CameraControl.gd
+++ CameraControl.gd
@@ -1,12 +1,32 @@
 extends Node2D
 
+signal lose_1
+signal lose_2
+
 var num_players = 1
+
+var is_game_continuing = false
+
+func start_game(): 
+	is_game_continuing = true
 
 func _process(delta):
 	var snake_1_pos = get_parent().get_node("snakehead1").position
 	if num_players == 2:
 		var snake_2_pos = get_parent().get_node("snakehead2").position
-		position.y = max(position.y, 
-			0.3*min(snake_1_pos.y, snake_2_pos.y) + 0.7*max(snake_1_pos.y, snake_2_pos.y))
+		var top_pos = min(snake_1_pos.y, snake_2_pos.y)
+		var bot_pos = max(snake_1_pos.y, snake_2_pos.y)
+		var mid_pos = 0.3*top_pos + 0.7*bot_pos
+		position.y = mid_pos
+		
+		if is_game_continuing:
+			if mid_pos - snake_1_pos.y > 200: 
+				is_game_continuing = false
+				emit_signal("lose_1")
+			if mid_pos - snake_2_pos.y > 200: 
+				is_game_continuing = false
+				emit_signal("lose_2")
+		
+		# print(snake_1_pos.y - mid_pos)
 	else:
 		position.y = snake_1_pos.y
---
+++ game_control.gd
@@ -0,0 +1,54 @@
+extends Node2D
+
+var is_game_over = false
+
+export var num_players = 2
+
+func _ready(): 
+	start_game()
+
+func _process(delta):
+	if is_game_over:
+		if Input.is_action_just_pressed("action_restart"):
+			get_tree().paused = false
+			get_tree().reload_current_scene()
+			
+func start_game(): 
+	$CameraControl.num_players = num_players
+	$trail.num_players = num_players
+	if num_players == 1: 
+		$snakehead2.queue_free()
+	$CameraControl.start_game()
+	$trail.start_game()
+
+func _on_snakehead1_collide():
+	print("Snakehead 1 game end!")
+	snake_1_loses()
+
+func _on_snakehead2_collide():
+	print("Snakehead 2 game end!")
+	snake_2_loses()
+
+func _on_CameraControl_lose_1():
+	print("Camera 1 game end!")
+	snake_1_loses()
+
+func _on_CameraControl_lose_2():
+	print("Camera 2 game end!")
+	snake_2_loses()
+
+func snake_1_loses(): 
+	get_tree().paused = true
+	$CameraControl/Winscreen.visible = true
+	var winscreenlabel = $CameraControl/Winscreen/Label
+	winscreenlabel.text = "Player 2\nWins!"
+	winscreenlabel.set("custom_colors/font_color", Color8(178,57,15))
+	is_game_over = true
+
+func snake_2_loses(): 
+	get_tree().paused = true
+	var winscreenlabel = $CameraControl/Winscreen/Label
+	$CameraControl/Winscreen.visible = true
+	winscreenlabel.text = "Player 1\nWins!"
+	winscreenlabel.set("custom_colors/font_color", Color(0.1,0.5,0.1))
+	is_game_over = true
