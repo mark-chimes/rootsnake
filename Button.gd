18d267707fb086500b0f337509804c6deb8c605e
user <user@goliath.dbai.tuwien.ac.at>
2023-02-05 15:40:12 +0100

    Credits and Help

--- Button.gd
+++ Button.gd
@@ -8,14 +8,14 @@ func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
 			print("LEFT CLICK " + button_text)
-			modulate = Color(1,1,1)
+			modulate = Color(0.6,0,0)
 			emit_signal("button_click")
 
 func _on_Area2D_mouse_entered():
 	modulate = Color(0,1,0)
 
 func _on_Area2D_mouse_exited():
-	modulate = Color(1,1,1)
+	modulate = Color(0.6,0,0)
 	
 func _ready():
 	$Label.text = button_text
